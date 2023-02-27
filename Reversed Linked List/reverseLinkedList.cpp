4364c24cd59215b5871c16a4eacb8fc92f69ea70
Prajwal77777 <96177425+Prajwal77777@users.noreply.github.com>
2023-02-27 21:08:35 +0545

    Create reverseLinkedList.cpp

---
+++ Reversed Linked List/reverseLinkedList.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+          ListNode *prev = NULL, *cur=head, *tmp;
+        while(cur){
+            tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+    }
+};
