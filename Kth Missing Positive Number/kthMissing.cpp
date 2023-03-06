fbf2b6c696fe6ba6404dcd87a6bd9c1aaaa19ed5
Prajwal77777 <96177425+Prajwal77777@users.noreply.github.com>
2023-03-06 23:01:48 +0545

    Create kthMissing.cpp

---
+++ Kth Missing Positive Number/kthMissing.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int left = 0; 
+        int right = arr.size();
+        while(left < right){
+           int mid = (left + right) / 2;
+            if(arr[mid] - mid -1 < k){
+                left = mid + 1;
+
+            }else{
+                right = mid;
+            }
+        }
+        return k + left;
+        
+    }
+};
